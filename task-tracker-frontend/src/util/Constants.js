adc60699dc4b4f14699cfaa55c36f222be7f5ffd
Rajan kumar <krcpr007@gmail.com>
2024-01-22 14:08:40 +0530

    Update : Backend url added from .env file

--- task-tracker-frontend/src/util/Constants.js
+++ task-tracker-frontend/src/util/Constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = process.env.REACT_APP_BACKEND_URL | 'http://localhost:8080/api';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const TASK_LIST_SIZE = 20;
